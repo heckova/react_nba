60784988726212e1ae5ef8cd9da9828326692cd2
Alta <heckova.l@gmail.com>
2017-11-23 15:04:39 +0000

    initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter , Route } from "react-router-dom";
+
+//COMPONENTS
+import Header from "./header";
+import  Footer from "./footer";
+import Home from "./home";
+
+class App extends React.Component {
+    render(){
+        return(
+            <BrowserRouter>
+                <div>
+                   <Header/>
+                   <Route exact path="/" component={Home}/>
+                    <Footer/>
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
+
+export default App;
