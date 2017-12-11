e1c82fd686dd2a9dd92e58e64b9bb0942b52e4ce
Alta <heckova.l@gmail.com>
2017-12-11 14:01:34 +0000

    added subscription component

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,36 @@
 import React, {Component} from "react";
+//COMPONENTS
+import Featured from "./featured";
+import Subscription from "./subscription";
+
+const URL_HOME = "http://localhost:3004/home";
+
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            home: ""
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL_HOME, {method: "GET"})
+            .then(response => response.json())
+            .then(json => {
+                this.setState({
+                    home: json
+                })
+            })
+    }
 
     render() {
         return (
-            <div>Home</div>
+            <div>
+                <Featured slides={this.state.home.slider}/>
+                <Subscription/>
+            </div>
         )
     }
 }
