d331af84030ab7bbe4768c1bc83d25984e6d334d
Alta <heckova.l@gmail.com>
2018-01-03 15:17:53 +0000

    created route team

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Header from "./header";
 import  Footer from "./footer";
 import Home from "./home";
 import Teams from "./teams";
+import Team from "./team";
 
 class App extends React.Component {
     render(){
@@ -15,6 +16,7 @@ class App extends React.Component {
                    <Header/>
                    <Route exact path="/" component={Home}/>
                     <Route exact path="/teams" component={Teams}/>
+                    <Route exact path="/team/:id" component={Team}/>
                     <Footer/>
                 </div>
             </BrowserRouter>
